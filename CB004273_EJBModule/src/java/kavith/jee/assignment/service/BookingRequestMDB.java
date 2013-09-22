649ffd75d27e5d4f362c67067cec22810d58a06e
Kavith Thiranga Lokuhewage <rc404mekt@gmail.com>
2013-09-23 02:01:40 +0530

    implemented MDB.

--- CB004273_EJBModule/src/java/kavith/jee/assignment/service/BookingRequestMDB.java
+++ CB004273_EJBModule/src/java/kavith/jee/assignment/service/BookingRequestMDB.java
@@ -4,10 +4,18 @@
  */
 package kavith.jee.assignment.service;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.ActivationConfigProperty;
+import javax.ejb.EJB;
 import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import kavith.jee.assignment.utils.BookingDetails;
 
 /**
  *
@@ -18,11 +26,21 @@ import javax.jms.MessageListener;
     @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "FilghtBookingService")
 })
 public class BookingRequestMDB implements MessageListener {
-    
+    @EJB
+    private AirlineAdminServiceBeanRemote airlineAdminServiceBean;
+ 
     public BookingRequestMDB() {
     }
     
     @Override
     public void onMessage(Message message) {
+        try {
+            if (message instanceof ObjectMessage) {
+                BookingDetails bd = (BookingDetails)((ObjectMessage)message).getObject();
+                airlineAdminServiceBean.placeABooking(bd);
+            }
+        } catch (JMSException ex) {
+            Logger.getLogger(BookingRequestMDB.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
